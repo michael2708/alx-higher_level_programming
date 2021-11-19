e07a7731ad8c09ee9940a0a3a72c1cb405d8f90a
michael2708 <akomolafem@gmail.com>
2021-11-18 19:05:06 -0800

    adds

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    length = len(argv)
+    if length == 1:
+        print("0 arguments.")
+    elif length == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{:d} arguments:".format(length - 1))
+        for idx, arg in enumerate(argv[1:]):
+            print("{}: {}".format(idx + 1, arg))
