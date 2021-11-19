e608cbb1e2733cf2066b44a86014e80432700b60
michael2708 <akomolafem@gmail.com>
2021-11-18 21:13:05 -0800

    adds

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    if len(argv) == 1:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        op = str(argv[2])
+        b = int(argv[3])
+        operator = {"+": add(a, b),
+                    "-": sub(a, b),
+                    "*": mul(a, b),
+                    "/": div(a, b)}
+        if op in operator:
+            print("{:d} {:s} {:d} = {:d}".format(a, op, b, operator[op]))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
