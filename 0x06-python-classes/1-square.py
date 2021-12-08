5c2c3e78280e8246b209e41c89b3b03d81599b95
michael2708 <akomolafem@gmail.com>
2021-12-08 14:48:58 -0800

    adds

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Module 1-square """
+
+
+class Square:
+    """
+    class Square definition
+    Attribute:
+        size : size of a side in square
+    """
+    def __init__(self, size):
+        """
+        Initializes square
+        Attributes:
+            size: size of a side of square
+        """
+        self.__size = size
