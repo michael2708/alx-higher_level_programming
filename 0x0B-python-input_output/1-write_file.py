08472c0f2b65c2ba486bc07fdafc2ca8312e6174
michael2708 <akomolafem@gmail.com>
2021-12-21 15:41:18 -0800

    add

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ Module 1-write_file.py
 
 def write_file(filename="", text=""):
     """ function that writes a string to a text file """
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
