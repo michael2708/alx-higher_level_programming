8b40a23b699c2922569d389cbf35d9ea454bf94d
michael2708 <akomolafem@gmail.com>
2021-12-21 14:45:54 -0800

    add

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,5 @@ Module 0-read_file.py
 
 def read_file(filename=""):
     """ function that reads a text file """
-    with open(filename) as f:
-        readfile = f.read()
-    return readfile
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
