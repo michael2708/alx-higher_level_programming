32c130a1dc48c0ec1d978f7d8aa913f2d760036b
michael2708 <akomolafem@gmail.com>
2022-01-03 09:41:09 -0800

    adds

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Model Contains Base Class
+"""
+
+
+class Base():
+    """
+    defines class Base
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize id, increment class attribute if no id and set as id"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
